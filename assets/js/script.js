8ecf785cf7302e5de1a517e6eb116d354690e366
Zoltan Kepes <zoltan.the.kepes@gmail.com>
2021-12-08 18:09:35 +0000

    fix:substraction code

--- assets/js/script.js
+++ assets/js/script.js
@@ -76,7 +76,7 @@ function calculateCorrectAnswer() {
     } else if (operator === "x") {
         return [operand1 * operand2, "multiply"];
     } else if (operator === "-") {
-        return [operand1, "subtract"];
+        return [operand1 - operand2, "subtract"];
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
@@ -113,7 +113,6 @@ function displayAdditionQuestion(operand1, operand2) {
 }
 
 function displaySubtractQuestion(operand1, operand2) {
-
     document.getElementById('operand1').textContent = operand1 + operand2;
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "-";
